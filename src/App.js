0a74468996fcede3098a33eba12f29f9a601eb9f
Eldad Shneor <38609871+tkss15@users.noreply.github.com>
2022-11-02 09:17:38 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,26 +3,41 @@ import './App.css';
 import { useFetch } from './hooks/useFetch';
 
 import BadGuess from './Components/BadGuess';
+import DataList from './Components/DataList';
+// import SearchArea from './Components/SearchArea';
 
 function App() {
   const {data,isPending,error} = useFetch('http://localhost:3000/champion')
   const [srcChampion, setChampion] = useState('')
   const [newdata, setData] = useState([])
-  const [randomChampion, setRandomChampion] = useState(null)
+  const [randomChampion, setRandomChampion] = useState({
+    name:"",
+    quote:"",
+    title:""
+  })
   const [isWinner, setIsWinner] = useState(false)
   const [badChampion, setbadChampion] = useState([])
   const [firstLoad, setFirstLoad] = useState(true)
+  const [guesses, setGuesses] = useState(0)
 
   const createNewGame = () => {
+    setData(data);
     setbadChampion([]);
     setIsWinner(false);
-    setRandomChampion(getRandomChampion());
+    getRandomChampion();
     setChampion('');
+    setGuesses(0);
     console.log('newgame', badChampion,isWinner,randomChampion,srcChampion)
   }
   const getRandomChampion = () => {
     const length = data.length;
-    return Math.floor(Math.random() * length);
+    const randomNumber = Math.floor(Math.random() * length);
+    setRandomChampion(
+      {
+        name:data[randomNumber].name,
+        title:data[randomNumber].title,
+        quote:data[randomNumber].quote
+      })
   }
   useEffect(() => {
     if(firstLoad && data)
@@ -31,61 +46,58 @@ function App() {
       createNewGame()
     }
   }, [firstLoad,data])
-  useEffect(() => {
-    if(srcChampion === '')
-    { 
-      setData([]);
-    }
-    if(data && srcChampion)
-    {
-      setData(data.filter((champion) => ((champion.name).toLowerCase()).includes(srcChampion.toLowerCase())))
-    }
-  }, [data,srcChampion])
 
   const handleGuess = (e) => {
-    e.preventDefault();
-    if(srcChampion === data[randomChampion].name)
+    //e.preventDefault();
+    if(srcChampion === randomChampion.name)
     {
       setIsWinner(true)
       setTimeout(() => createNewGame(), 2000);
     }
     else
     {
+      setGuesses((prevGuess) => prevGuess + 1)
       setbadChampion((prevchampion) => {
         return [...prevchampion, srcChampion]
       })
+      setData((prevData) => {
+        return prevData.filter((champion) => {
+          if(champion.name === srcChampion)
+            return false;
+          return true;
+        })
+      })
+      console.log(newdata)
     }
   }
   return (
     <div className="App">
       <h1>Todays Abillty Quote:</h1>
-      {randomChampion && <h2>{data[randomChampion].quote}</h2>}
+      {randomChampion && <h2>{randomChampion.quote}</h2>}
             <h3>Search bar</h3>
-        <form className='search-area' onSubmit={handleGuess}>
+       <form className='search-area' onSubmit={handleGuess}>
             <label>
-              <input 
+              <input
               type='text'
               id='search'
               onChange={(e) => setChampion(e.target.value)}
               value={srcChampion}
               autoComplete="off"></input>
             </label>
-            {error && <p>{error}</p>}
-            {isPending && <p>Loading...</p>}
-            {srcChampion && <div className='display-champions'>
-              {newdata && newdata.map((champion) => (
-              <em key={champion.name} onClick={() => setChampion(champion.name)}>{champion.name}</em>
-            ))}</div>}
-
+              <DataList
+              data={newdata}
+              isPending={isPending}
+              error={error}
+              search={srcChampion}
+              setChampion={setChampion}/>
             <button>Guess !</button>
         </form>
       {isWinner && <p>Won !</p>}
 
-      <BadGuess badChampion={badChampion}>
-        <h2>Wrong Guesses</h2>
-      </BadGuess>
+      {randomChampion && <BadGuess championsQuote={randomChampion.title} badChampion={badChampion} guessAmount={guesses}/>}
     </div>
   );
 }
 
 export default App;
+
